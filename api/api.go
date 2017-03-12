59429363a0d30f496ff7ec64babc7fa7149634dd
willyingling <willyingling@gmail.com>
2017-03-12 11:20:50 -0600

    Added context to api

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -28,10 +29,10 @@ type Reply struct {
 }
 
 type Resource interface {
-	Get(url.Values) (HdrReply, chan Reply)
-	Put(url.Values) (HdrReply, chan Reply)
-	Post(url.Values) (HdrReply, chan Reply)
-	Delete(url.Values) (HdrReply, chan Reply)
+	Get(context.Context, url.Values) (HdrReply, chan Reply)
+	Put(context.Context, url.Values) (HdrReply, chan Reply)
+	Post(context.Context, url.Values) (HdrReply, chan Reply)
+	Delete(context.Context, url.Values) (HdrReply, chan Reply)
 }
 
 type (
@@ -41,19 +42,19 @@ type (
 	DeleteNotAllowed struct{}
 )
 
-func (GetNotAllowed) Get(vals url.Values) (HdrReply, chan Reply) {
+func (GetNotAllowed) Get(ctx context.Context, vals url.Values) (HdrReply, chan Reply) {
 	return HdrReply{Code: 405}, nil
 }
 
-func (PutNotAllowed) Put(vals url.Values) (HdrReply, chan Reply) {
+func (PutNotAllowed) Put(ctx context.Context, vals url.Values) (HdrReply, chan Reply) {
 	return HdrReply{Code: 405}, nil
 }
 
-func (PostNotAllowed) Post(vals url.Values) (HdrReply, chan Reply) {
+func (PostNotAllowed) Post(ctx context.Context, vals url.Values) (HdrReply, chan Reply) {
 	return HdrReply{Code: 405}, nil
 }
 
-func (DeleteNotAllowed) Delete(vals url.Values) (HdrReply, chan Reply) {
+func (DeleteNotAllowed) Delete(ctx context.Context, vals url.Values) (HdrReply, chan Reply) {
 	return HdrReply{Code: 405}, nil
 }
 
@@ -71,6 +72,7 @@ func (api *API) requestHandlerFunc(resource Resource) http.HandlerFunc {
 			datac chan Reply
 			code  HdrReply
 		)
+		ctx, cf := context.WithCancel(context.Background())
 		log.Printf("--Request From:%s --", req.RemoteAddr)
 		req.ParseForm()
 		method := req.Method
@@ -80,13 +82,13 @@ func (api *API) requestHandlerFunc(resource Resource) http.HandlerFunc {
 		vals["remoteaddr"] = ip
 		switch method {
 		case GET:
-			code, datac = resource.Get(vals)
+			code, datac = resource.Get(ctx, vals)
 		case PUT:
-			code, datac = resource.Put(vals)
+			code, datac = resource.Put(ctx, vals)
 		case POST:
-			code, datac = resource.Post(vals)
+			code, datac = resource.Post(ctx, vals)
 		case DELETE:
-			code, datac = resource.Delete(vals)
+			code, datac = resource.Delete(ctx, vals)
 		}
 		if code.Extra != "" { //he have a uuid for continuous pulling
 			rw.Header().Set("Next-Pull-ID", code.Extra)
@@ -96,12 +98,21 @@ func (api *API) requestHandlerFunc(resource Resource) http.HandlerFunc {
 		rw.WriteHeader(code.Code)
 		if datac != nil { // we got a proper channel to get datafrom
 			//go func(dc <-chan Reply) { // fire a goroutine that will end upon the chan getting closed
-			for r := range datac {
-				if r.Err == nil {
-					rw.Write(r.Data)
-				} else {
-					log.Printf("Error in received from data channel:%s\n", r.Err)
-					rw.Write([]byte(fmt.Sprintf("%s\n", r.Err)))
+			reqOpen := true
+			for reqOpen {
+				select {
+				case r, reqOpen := <-datac:
+					if reqOpen {
+						if r.Err == nil {
+							rw.Write(r.Data)
+						} else {
+							log.Printf("Error in received from data channel:%s\n", r.Err)
+							rw.Write([]byte(fmt.Sprintf("%s\n", r.Err)))
+						}
+					}
+				case <-rw.(http.CloseNotifier).CloseNotify():
+					cf()
+					reqOpen = false
 				}
 			}
 			//}(datac)
